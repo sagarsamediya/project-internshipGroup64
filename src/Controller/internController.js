33c4b0ccf20993ed6d700b00a7d354fae8356568
sagarsamediya <sagarsamediya17@.com>
2022-06-29 12:19:53 +0530

    sagar

--- src/Controller/internController.js
+++ src/Controller/internController.js
@@ -1,1 +1,25 @@
 const internModel = require("../Model/internModel")
+
+const isBodyExist = function(data){
+    return Object.keys(data).length>0
+};
+
+const isValid = function (value) {
+    if (typeof value === 'undefined' || value === null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
+    return true;
+};
+
+
+const createIntern = async function (req, res){
+    try{
+        let data = req.body
+        if(!isBodyExist(data)){
+            return  res.status(400).send({status:false,message:"Body can't be empty"})
+         }
+ 
+    }catch(err){
+          console.log(err)
+          res.status(500).send({status: false , msg: "err message"})
+    }
+}
