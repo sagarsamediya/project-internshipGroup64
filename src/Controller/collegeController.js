492c4ae14ba79949615a42735a0668123ab97fc6
sagarsamediya <sagarsamediya17@.com>
2022-06-29 15:58:40 +0530

    sagar

--- src/Controller/collegeController.js
+++ src/Controller/collegeController.js
@@ -21,57 +21,55 @@ const createCollege = async function (req, res) {
   
       // Body Validation
   
-      if (!isBodyExist(data)) {
-        return res
-          .status(400)
-          .send({ status: false, message: "Please provide college details" });
-      }
+      if (!isBodyExist(data)) {  return res  .status(400) .send({ status: false, message: "Please provide college details" }); }
   
       // Destructuring body
   
-      const { name, fullName, logoLink } = data;
+       const { name, fullName, logoLink } = data;
 
       if (!data.name) return res.status(400).send({ status: false, message: "name is required" })
       if (!data.fullName) return res.status(400).send({ status: false, message: "fullName is required" })
       if (!data.logoLink) return res.status(400).send({ status: false, message: "logoLink is required" })
   
       // Validation Starts
-      if (!isValid(name)) {
-        return res
-          .status(400)
-          .send({ status: false, message: "Please provide valid college name" });
-      }
+      if (!isValid(name)) return res.status(400).send({ status: false, message: "Please provide valid college name" });
+      
   
       let checkName = await collegeModel.findOne({ name: data.name })
               if (checkName) return res.status(400).send({ msg: "College Name already exist" })
   
-      if (!isValid(fullName)) {
-        return res.status(400).send({
-          status: false,
-          message: "Please provide valid fullName of the college",
-        });
-      }
+      if (!isValid(fullName)) {return res.status(400).send({status: false, message: "Please provide valid fullName of the college",});
+      
+    }
   
-      if (!isValid(logoLink)) {
-        return res
-          .status(400)
-          .send({ status: false, message: "Please provide valid url" });
-      }
+      if (!isValid(logoLink)) { return res.status(400).send({ status: false, message: "Please provide valid url" });
+      
+    }
 
       // Creating College
 
     const college = await collegeModel.create(data);
 
-    res.status(201).send({
-      status: true,
-      message: "College successfully created",
-      data: college,
-    });
+    res.status(201).send({ status: true, message: "College successfully created", data: college });
+
   } catch (error) {
     res.status(500).send({ status: false, message: error.message });
   }
 }
 
+// ----------------------**-----------college details-------------------------***---------------------------------------**--------------
 
+const collegeDetails = async function(req, res) {
+    try{
+        let collegeName = req.query
+
+        
+
+
+    }catch(err){
+        console.log(err)
+        res.status(500).send({status: false , msg: "err message"})
+    }
+}
 
 module.exports.createCollege = createCollege
